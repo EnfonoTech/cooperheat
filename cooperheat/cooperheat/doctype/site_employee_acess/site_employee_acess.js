a18ac1acc1c10afdf5250e9685a34a505eea6bad
Neha Fathima <nehaputhur443@gmail.com>
2026-06-18 16:38:36 +0530

    feat:a added the employee site acess

---
+++ cooperheat/cooperheat/doctype/site_employee_acess/site_employee_acess.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, enfonotechnology and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Site Employee Acess", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ cooperheat/cooperheat/doctype/site_employee_acess/test_site_employee_acess.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, enfonotechnology and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSiteEmployeeAcess(FrappeTestCase):
+	pass
